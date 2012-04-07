56718387bba91e253f20d89e5dbe0ecff4d7485a
Christopher Schmidt <christopher.schmidt@nokia.com>
2012-04-07 07:29:52 -0400

    More changes.

--- review/urls.py
+++ review/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 
-urlpatterns = patterns('unconferencereviews.review',
+urlpatterns = patterns('unconferencereviews.review.views',
     # Examples:
     url(r'^$', 'home', name='home'),
     
--- review/views.py
+++ review/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render_to_response
 
 def home(request):
     
-    return render_to_respons("home.html")
+    return render_to_response("home.html")
